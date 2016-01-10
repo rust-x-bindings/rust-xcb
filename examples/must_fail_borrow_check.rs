842829defabde32e8a69cbb587dcfe957b373b8e
Remi THEBAULT <remi.thebault@gmail.com>
2016-01-10 16:13:21 +0100

    examples/must_fail_borrow_check.rs
    checks if Setup lifetime is correctly tied to Connection lifetime

---
+++ examples/must_fail_borrow_check.rs
@@ -0,0 +1,24 @@
+extern crate xcb;
+
+use std::iter::{Iterator};
+use xcb::base::*;
+
+fn main() {
+
+    let mut setup;
+    let mut screen_num;
+    {
+        let (conn, sn) = Connection::connect();
+        setup = conn.get_setup();
+        screen_num = sn;
+    }
+
+    let mut screen = setup.roots().nth(screen_num as usize).unwrap();
+
+    println!("");
+    println!("Informations of screen {}:", screen.root());
+    println!("  width..........: {}", screen.width_in_pixels());
+    println!("  height.........: {}", screen.height_in_pixels());
+    println!("  white pixel....: {:x}", screen.white_pixel());
+    println!("  black pixel....: {:x}", screen.black_pixel());
+}
