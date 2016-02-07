d7a60eec603a89b06748a454f4fd6e6a0abc9788
Remi THEBAULT <remi.thebault@gmail.com>
2016-02-07 03:13:13 +0100

    travis script for installing xcb
    xkb does not seem present in travis environment

---
+++ scripts/install_xcb.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+set -x
+
+wget http://xcb.freedesktop.org/dist/xcb-proto-1.11.tar.bz2
+wget http://xcb.freedesktop.org/dist/libxcb-1.11.1.tar.bz2
+
+tar -xjf xcb-proto-1.11.tar.bz2
+cd xcb-proto-1.11
+./configure
+make
+sudo make install
+cd ..
+
+tar -xjf libxcb-1.11.1.tar.bz2
+cd libxcb-1.11.1
+./configure --enable-xkb --enable-xinput --enable-selinux
+make
+sudo make install
+cd..
