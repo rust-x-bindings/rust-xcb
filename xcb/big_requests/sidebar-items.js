5193e072e5282f098ae288c8bd7f73b5be4aa8b5
Remi THEBAULT <remi.thebault@gmail.com>
2016-03-29 02:17:18 +0200

    update

---
+++ xcb/big_requests/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"constant":[["ENABLE",""],["MAJOR_VERSION",""],["MINOR_VERSION",""]],"fn":[["enable",""],["enable_unchecked",""],["id",""]],"type":[["EnableCookie",""],["EnableReply",""]]});
