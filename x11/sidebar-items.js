d112b328c2951be10110f9cabffbe02518e40497
Remi THEBAULT <remi.thebault@gmail.com>
2016-04-03 15:32:19 +0200

    update

--- x11/sidebar-items.js
+++ x11/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["glx",""],["keysym",""],["xcursor",""],["xf86vmode",""],["xfixes",""],["xinerama",""],["xinput",""],["xinput2",""],["xlib",""],["xmu",""],["xrecord",""],["xrender",""],["xt",""],["xtest",""]]});
+initSidebarItems({"mod":[["glx",""],["keysym",""],["xcursor",""],["xf86vmode",""],["xfixes",""],["xinerama",""],["xinput",""],["xinput2",""],["xlib",""],["xmu",""],["xrandr",""],["xrecord",""],["xrender",""],["xt",""],["xtest",""]]});
