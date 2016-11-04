e7620062f2b127964188420812859a7c9ce12d05
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-11-04 23:33:05 +0100

    Removed cast-to-union code for PODs
    It's not strictly needed in most cases and doesn't work cleanly anyway.
    More discussion on this to come.

--- rs_client.py
+++ rs_client.py
@@ -1203,15 +1203,6 @@ def _rs_union_accessor(typeobj, field):
                     _r('_ptr')
             _r('}')
         _r('}')
-        if field.type.rs_is_pod:
-            _r('pub fn from_%s(%s: %s) -> %s {', field.rs_field_name,
-                    field.rs_field_name, field.rs_field_type, typeobj.rs_type)
-            with _r.indent_block():
-                _r('unsafe {')
-                with _r.indent_block():
-                        _r('std::mem::transmute(%s)', field.rs_field_name);
-                _r('}')
-            _r('}')
 
 
 
