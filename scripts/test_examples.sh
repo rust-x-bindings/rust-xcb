80f4ad318495304c15a9631100f16c38b5287b78
Remi THEBAULT <remi.thebault@gmail.com>
2016-11-14 19:02:22 +0100

    testing example randr_crtc_listen

--- scripts/test_examples.sh
+++ scripts/test_examples.sh
@@ -46,8 +46,9 @@ test_success cargo build --example basic_window
 test_success cargo build --example drawing
 test_success cargo build --example draw_root_window
 test_success cargo build --example ffi_randr_crtc_info --features randr
-test_success cargo build --example randr_crtc_info --features randr
 test_success cargo build --example randr_screen_info --features randr
+test_success cargo build --example randr_crtc_info --features randr
+test_success cargo build --example randr_crtc_listen --features randr
 test_success cargo build --example ffi_xkb_init --features xkb
 test_success cargo build --example xkb_init --features xkb
 test_success cargo build --example threaded_window --features thread
