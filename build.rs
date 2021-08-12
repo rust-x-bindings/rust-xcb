ef881831fcbd0a52975ecb73de3650e6831dd0fd
AsmPrgmC3 <asmprgmc3@gmail.com>
2021-08-12 15:40:16 +0200

    Enable cross compilation from Windows

--- build.rs
+++ build.rs
@@ -6,8 +6,26 @@ use std::env;
 use std::cmp;
 use std::path::{Path, PathBuf};
 use std::fs;
-use std::os::unix::fs::MetadataExt;
 use std::process::Command;
+
+
+#[cfg(target_family = "unix")]
+fn mtime<P: AsRef<Path>>(path: P) -> io::Result<i64> {
+    use std::os::unix::fs::MetadataExt;
+    fs::metadata(path).map(|m| m.mtime())
+}
+
+#[cfg(target_family = "unix")]
+const PYTHON_NAME: &str = "python3";
+
+#[cfg(target_family = "windows")]
+fn mtime<P: AsRef<Path>>(path: P) -> io::Result<i64> {
+    use std::os::windows::fs::MetadataExt;
+    fs::metadata(path).map(|m| m.last_write_time() as i64)
+}
+
+#[cfg(target_family = "windows")]
+const PYTHON_NAME: &str = "python";
 
 
 fn visit_xml<F>(xml_dir: &Path, cb: F) -> io::Result<()>
@@ -34,11 +52,7 @@ fn xml_to_rs (rs_dir: &Path, xml_file: &Path) -> PathBuf {
 }
 
 fn optional_mtime (path: &Path, default: i64) -> i64 {
-    if let Ok(md) = fs::metadata(&path) {
-        md.mtime()
-    } else {
-        default
-    }
+    mtime(path).unwrap_or(default)
 }
 
 fn main() {
@@ -50,14 +64,14 @@ fn main() {
     let src_ffi_dir = Path::new(&src_dir).join("ffi");
     let out_dir = env::var("OUT_DIR").unwrap();
 
-    let r_client_mtime = fs::metadata(&r_client).unwrap().mtime();
-    let build_rs_mtime = fs::metadata(&build_rs).unwrap().mtime();
+    let r_client_mtime = mtime(&r_client).unwrap();
+    let build_rs_mtime = mtime(&build_rs).unwrap();
     let ref_mtime = cmp::max(r_client_mtime, build_rs_mtime);
 
     visit_xml(&xml_dir, |xml_file: &Path| -> io::Result<()> {
         let src_file = xml_to_rs(&src_dir, &xml_file);
         let ffi_file = xml_to_rs(&src_ffi_dir, &xml_file);
-        let xml_file_mtime = fs::metadata(&xml_file)?.mtime();
+        let xml_file_mtime = mtime(&xml_file)?;
         let src_file_mtime = optional_mtime(&src_file, 0);
         let ffi_file_mtime = optional_mtime(&ffi_file, 0);
 
@@ -65,7 +79,7 @@ fn main() {
 
         if ref_mtime > src_file_mtime || ref_mtime > ffi_file_mtime {
 
-            let status = Command::new("python3")
+            let status = Command::new(PYTHON_NAME)
                     .arg("-B")      // disable __pycache__ dir that messes with cargo
                     .arg(&r_client)
                     .arg("-o").arg(&out_dir)
