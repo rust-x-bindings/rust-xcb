3f7cec6c4356bb3a7ad6c04951eaebdb76ad7756
rtbo <remi.thebault@gmail.com>
2021-10-31 13:16:53 +0000

    Deploying to gh-pages from @ rtbo/rust-xcb@79521eaaefb2069433464f1c6a3d3c33f1b716d3 🚀

--- branches/xcb-1.14/xcb/bigreq/sidebar-items.js
+++ branches/xcb-1.14/xcb/bigreq/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["MAJOR_VERSION",""],["MINOR_VERSION",""],["VERSION_STRING",""],["XNAME",""]],"struct":[["Enable",""],["EnableCookie",""],["EnableReply",""],["UncheckedEnableCookie",""]]});
+initSidebarItems({"constant":[["MAJOR_VERSION",""],["MINOR_VERSION",""],["VERSION_STRING",""],["XNAME",""]],"fn":[["get_extension_data",""],["prefetch_extension_data",""]],"struct":[["Enable",""],["EnableCookie",""],["EnableReply",""],["UncheckedEnableCookie",""]]});
--- branches/xcb-1.14/xcb/ffi/base/sidebar-items.js
+++ branches/xcb-1.14/xcb/ffi/base/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["xcb_connection_t","@brief XCB Connection structure."]],"struct":[["xcb_generic_error_t","Generic error"]]});
+initSidebarItems({"enum":[["xcb_connection_t","@brief XCB Connection structure."]],"struct":[["xcb_generic_error_t","Generic error"],["xcb_generic_event_t","@brief Generic event."]]});
