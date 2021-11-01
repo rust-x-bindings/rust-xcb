9e09e7fe086de63f8fc557c0c29983d10bc9f734
rtbo <remi.thebault@gmail.com>
2021-11-01 10:32:19 +0000

    Deploying to gh-pages from @ rtbo/rust-xcb@48a6448c6e00f80dce94d7120851436e27704a07 🚀

--- branches/xcb-1.14/xcb/ffi/base/sidebar-items.js
+++ branches/xcb-1.14/xcb/ffi/base/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["xcb_connection_t","@brief XCB Connection structure."]],"struct":[["xcb_generic_error_t","Generic error"],["xcb_generic_event_t","@brief Generic event."]]});
+initSidebarItems({"enum":[["xcb_connection_t","@brief XCB Connection structure."]],"struct":[["xcb_ge_generic_event_t",""],["xcb_generic_error_t","Generic error"],["xcb_generic_event_t","@brief Generic event."]]});
