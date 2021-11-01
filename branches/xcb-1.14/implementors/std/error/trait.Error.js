30dba3e4b122cb920b378ddc5690157f43614e5c
rtbo <remi.thebault@gmail.com>
2021-11-01 18:00:36 +0000

    Deploying to gh-pages from @ rtbo/rust-xcb@4f08328cffc924e9638d1a4f3c08871c2de6eb1c 🚀

--- branches/xcb-1.14/implementors/std/error/trait.Error.js
+++ branches/xcb-1.14/implementors/std/error/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["xcb"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.56.0/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"xcb/base/enum.ConnError.html\" title=\"enum xcb::base::ConnError\">ConnError</a>","synthetic":false,"types":["xcb::base::ConnError"]}];
+implementors["xcb"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.56.1/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"xcb/base/enum.ConnError.html\" title=\"enum xcb::base::ConnError\">ConnError</a>","synthetic":false,"types":["xcb::base::ConnError"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
