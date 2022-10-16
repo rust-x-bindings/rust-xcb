9d5024dfb6998fd7d5eb8da11486df5513a3611a
rogarb <rogarb@rgarbage.fr>
2022-10-16 21:40:19 +0200

    Add example for drawing on the root window (v1)

---
+++ examples/draw_root_window.rs
@@ -0,0 +1,81 @@
+use xcb::x;
+
+fn main() -> xcb::Result<()> {
+    let rectangles: &[x::Rectangle] = &[x::Rectangle {
+        x: 200,
+        y: 200,
+        width: 400,
+        height: 400,
+    }];
+    let (conn, screen_num) = xcb::Connection::connect(None).unwrap();
+    let setup = conn.get_setup();
+    let screen = setup.roots().nth(screen_num as usize).unwrap();
+
+    /* get the root window of the screen */
+    let window = screen.root();
+
+    /* Request modification of the root window attributes: background color and
+     * event mask */
+    conn.send_request(&x::ChangeWindowAttributes {
+        window,
+        value_list: &[
+            x::Cw::BackPixel(screen.white_pixel()),
+            // events that will be waited for
+            x::Cw::EventMask(x::EventMask::EXPOSURE | x::EventMask::KEY_PRESS),
+        ],
+    });
+
+    let gc: x::Gcontext = conn.generate_id();
+    /* Request the creation of the graphical context */
+    conn.send_request(&x::CreateGc {
+        cid: gc,
+        drawable: x::Drawable::Window(window),
+        value_list: &[
+            x::Gc::Function(x::Gx::Xor),
+            x::Gc::Foreground(screen.white_pixel()),
+            x::Gc::Background(screen.black_pixel()),
+            x::Gc::LineWidth(1),
+            x::Gc::LineStyle(x::LineStyle::OnOffDash),
+            x::Gc::GraphicsExposures(false),
+        ],
+    });
+
+    /* Flush the requests */
+    conn.flush()?;
+    loop {
+        let event = match conn.wait_for_event() {
+            Err(xcb::Error::Connection(xcb::ConnError::Connection)) => {
+                // graceful shutdown, likely "x" close button clicked in title bar
+                break Ok(());
+            }
+            Err(err) => {
+                panic!("unexpected error: {:#?}", err);
+            }
+            Ok(event) => event,
+        };
+        match event {
+            xcb::Event::X(x::Event::Expose(_ev)) => {
+                let drawable = x::Drawable::Window(window);
+
+                /* Draw a rectangle on screen using the graphical context */
+                conn.send_request(&x::PolyRectangle {
+                    drawable,
+                    gc,
+                    rectangles,
+                });
+
+                /* We flush the request */
+                conn.flush()?;
+            }
+
+            xcb::Event::X(x::Event::KeyPress(key_press)) => {
+                println!("Key '{}' pressed", key_press.detail());
+                if key_press.detail() == 0x18 {
+                    // Q (on qwerty)
+                    break Ok(());
+                }
+            }
+            _ => {}
+        }
+    }
+}
