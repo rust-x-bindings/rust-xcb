e7726dfe1c263dd1857c09a380c749019ba2f744
rtbo <remi.thebault@gmail.com>
2023-04-23 20:40:18 +0000

    Deploying to gh-pages from @ rust-x-bindings/rust-xcb@5e374f8904ac8038bdb78e2ee882a4f4ccbd43f0 🚀

--- x11/xlib_xcb/sidebar-items.js
+++ x11/xlib_xcb/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":[["XEventQueueOwner",""]],"fn":[["XGetXCBConnection",""],["XSetEventQueueOwner",""]],"type":[["xcb_connection_t",""]]};
+window.SIDEBAR_ITEMS = {"enum":["XEventQueueOwner"],"fn":["XGetXCBConnection","XSetEventQueueOwner"],"type":["xcb_connection_t"]};
