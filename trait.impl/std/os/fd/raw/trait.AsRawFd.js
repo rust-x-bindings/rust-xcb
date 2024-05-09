462498d4f8b0b8d6daf0b6d112fd65be13e76d14
rtbo <remi.thebault@gmail.com>
2024-05-09 13:08:16 +0000

    Deploying to gh-pages from @ rust-x-bindings/rust-xcb@77a19722ce2f3d12773ec5068d31b1ce9cbf5f13 🚀

--- trait.impl/std/os/fd/raw/trait.AsRawFd.js
+++ trait.impl/std/os/fd/raw/trait.AsRawFd.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"xcb":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.2/std/os/fd/raw/trait.AsRawFd.html\" title=\"trait std::os::fd::raw::AsRawFd\">AsRawFd</a> for <a class=\"struct\" href=\"xcb/struct.Connection.html\" title=\"struct xcb::Connection\">Connection</a>"]]
+"xcb":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.78.0/std/os/fd/raw/trait.AsRawFd.html\" title=\"trait std::os::fd::raw::AsRawFd\">AsRawFd</a> for <a class=\"struct\" href=\"xcb/struct.Connection.html\" title=\"struct xcb::Connection\">Connection</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
